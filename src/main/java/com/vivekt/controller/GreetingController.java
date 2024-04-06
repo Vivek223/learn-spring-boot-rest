94edc2637d9d0349e1c94ff63b5391ac33fd8f18
viv_tiw <yt4fun2@gmail.com>
2024-04-06 21:00:20 +0300

    First controller added

---
+++ src/main/java/com/vivekt/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.vivekt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+
+    @GetMapping("/info")
+    public String greet(){
+        return "Welcome to the world of learning. We will learn Spring boot with Rest in this tutorial";
+    }
+}
