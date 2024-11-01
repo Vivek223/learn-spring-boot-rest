65d13f94187fe13ace89c32691f4060f8bfd0da7
vivek <tiwarivivek23@gmail.com>
2024-11-01 12:14:02 +0300

    Added two apis

--- src/main/java/com/vivekt/controller/GreetingController.java
+++ src/main/java/com/vivekt/controller/GreetingController.java
@@ -1,6 +1,10 @@
 package com.vivekt.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +14,11 @@ public class GreetingController {
     @GetMapping("/info")
     public String greet(){
         return "Welcome to the world of learning. We will learn Spring boot with Rest in this tutorial -- it1" ;
+    }
+
+    @PostMapping("/sqrt")
+    public ResponseEntity<Integer> sqrt(@RequestParam Integer num){
+        Integer result = num * num;
+        return ResponseEntity.ok(result);
     }
 }
