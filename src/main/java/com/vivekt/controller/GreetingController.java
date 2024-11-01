f0f4727c6e623d821ec83a29dff2b9d33e301a9b
vivek <tiwarivivek23@gmail.com>
2024-11-01 11:59:10 +0300

    updated it1

--- src/main/java/com/vivekt/controller/GreetingController.java
+++ src/main/java/com/vivekt/controller/GreetingController.java
@@ -9,6 +9,6 @@ public class GreetingController {
 
     @GetMapping("/info")
     public String greet(){
-        return "Welcome to the world of learning. We will learn Spring boot with Rest in this tutorial";
+        return "Welcome to the world of learning. We will learn Spring boot with Rest in this tutorial -- it1" ;
     }
 }
